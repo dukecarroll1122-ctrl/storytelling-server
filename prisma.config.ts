08ded26e771d8ae698102ba53e055be3f59d2477
Randy Carroll Jr <dukecarroll1122@gmail.com>
2026-06-18 12:10:48 -0400

    add migrations url to prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
     path: 'prisma/migrations',
+    url: process.env.DATABASE_URL,
   },
   datasource: {
     adapter: () => {
