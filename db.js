695d3ca904e5b954f35a3919485b9ff14a758259
Randy Carroll Jr <dukecarroll1122@gmail.com>
2026-06-18 12:22:40 -0400

    fix prisma client import path to client.js

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from './generated/prisma/index.js'
+import { PrismaClient } from './prisma/generated/prisma/client.js'
 import { PrismaPg } from '@prisma/adapter-pg'
 import pkg from 'pg'
 const { Pool } = pkg
